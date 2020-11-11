009fe38a25b08f6639a0907baa4de4fd9c6bfc24
notgurev <notgurev@gmail.com>
2020-11-11 06:00:14 +0300

    костыльная работа manual inputs (y почему-то null), сырой вывод в таблицу

--- src/main/java/lab3/beans/HitBean.java
+++ src/main/java/lab3/beans/HitBean.java
@@ -1,17 +1,57 @@
 package lab3.beans;
 
-import lombok.Data;
+import lab3.utils.HitChecker;
 
 import javax.faces.bean.ApplicationScoped;
 import javax.faces.bean.ManagedBean;
 
-@ManagedBean
+@ManagedBean(name = "hit") // todo это должно быть в отдельном файле
 @ApplicationScoped
-@Data
 public class HitBean {
-    private boolean[] xArray = new boolean[9]; // for regular input
-    private int x; // for canvas input
-    private float y; // both
-    private float r; // god help
+    private int x;
+    private Float y;
+    private float r;
     private boolean successful;
+
+    public HitBean() {
+    }
+
+    public HitBean(int x, Float y, float r) {
+        this.x = x;
+        this.y = y;
+        this.r = r;
+        successful = HitChecker.checkArea(x, y, r);
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public Float getY() {
+        return y;
+    }
+
+    public void setY(Float y) {
+        this.y = y;
+    }
+
+    public float getR() {
+        return r;
+    }
+
+    public void setR(float r) {
+        this.r = r;
+    }
+
+    public boolean isSuccessful() {
+        return successful;
+    }
+
+    public void setSuccessful(boolean successful) {
+        this.successful = successful;
+    }
 }
--- src/main/java/lab3/beans/HitsBean.java
+++ src/main/java/lab3/beans/HitsBean.java
@@ -2,21 +2,56 @@ package lab3.beans;
 
 import lombok.Data;
 
-
 import javax.faces.bean.ApplicationScoped;
 import javax.faces.bean.ManagedBean;
 import java.util.ArrayList;
 import java.util.List;
 
-@ManagedBean
+@ManagedBean(name = "hits")
 @ApplicationScoped
 @Data
 public class HitsBean {
-    private HitBean newHitBean = new HitBean();
+    private static final int[] X_VALUES = {-4, -3, -2, -1, 0, 1, 2, 3, 4};
     private final List<HitBean> hitBeansList = new ArrayList<>(); // Deque?
+    private boolean[] xArray = new boolean[9]; // for regular input
+    private int x; // for canvas input
+    private Float y; // both
 
-    public void registerHit() {
-        hitBeansList.add(newHitBean);
-        newHitBean = new HitBean();
+    public void registerHit(float radius, boolean singleX) {
+        y = 2f;
+        if (singleX) {
+            hitBeansList.add(new HitBean(x, y, radius));
+        } else {
+            for (int xChecked = 0; xChecked < xArray.length; xChecked++) {
+                System.out.println("TEST");
+                if (xArray[xChecked]) {
+                    hitBeansList.add(new HitBean(X_VALUES[xChecked], y, radius));
+                }
+            }
+        }
+    }
+
+    public boolean[] getxArray() {
+        return xArray;
+    }
+
+    public void setxArray(boolean[] xArray) {
+        this.xArray = xArray;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public Float getY() {
+        return y;
+    }
+
+    public void setY(Float y) {
+        this.y = y;
     }
 }
