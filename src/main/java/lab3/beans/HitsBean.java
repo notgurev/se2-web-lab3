97d716535cee41f8945b149a7e61645b64d9a050
notgurev <notgurev@gmail.com>
2020-11-13 23:28:57 +0300

    Валидация X

--- src/main/java/lab3/beans/HitsBean.java
+++ src/main/java/lab3/beans/HitsBean.java
@@ -1,21 +1,27 @@
 package lab3.beans;
 
+import lab3.utils.ArrayUtils;
+import lab3.validation.ValidatorY;
 import lombok.AccessLevel;
 import lombok.Data;
 import lombok.Getter;
-import lombok.Setter;
 
 import javax.faces.bean.ApplicationScoped;
 import javax.faces.bean.ManagedBean;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.ValidatorException;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
+
+import static javax.faces.application.FacesMessage.SEVERITY_WARN;
+import static lab3.utils.FacesUtils.addFacesMessage;
 
 @ManagedBean(name = "hits")
 @ApplicationScoped
 @Data
 public class HitsBean implements Serializable {
-    @Getter(AccessLevel.NONE)
     private static final int[] X_VALUES = {-4, -3, -2, -1, 0, 1, 2, 3, 4};
 
     private final List<HitBean> hitBeansList = new ArrayList<>(); // Deque?
@@ -33,7 +39,16 @@ public class HitsBean implements Serializable {
         hitBeansList.clear();
     }
 
+    public boolean validateManualInputs() {
+        if (!ArrayUtils.containsTrue(xArray)) { // no Xs chosen
+            addFacesMessage(SEVERITY_WARN, "Warning!", "Please select at least one X coordinate");
+            return false;
+        }
+        return true;
+    }
+
     public void submitManualInputHit(float radius) {
+        if (validateManualInputs()) return;
         for (int xChecked = 0; xChecked < xArray.length; xChecked++) {
             if (xArray[xChecked]) {
                 hitBeansList.add(new HitBean(X_VALUES[xChecked], y, radius));
