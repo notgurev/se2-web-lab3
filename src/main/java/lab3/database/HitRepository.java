62d13ea9bb6e68ce3a296bf3bbcc4e9f7f4afee1
notgurev <notgurev@gmail.com>
2020-11-16 04:38:34 +0300

    Repository interface, HitRepository implementation (empty)

---
+++ src/main/java/lab3/database/HitRepository.java
@@ -0,0 +1,23 @@
+package lab3.database;
+
+import lab3.beans.Hit;
+
+import java.util.List;
+
+public class HitRepository implements Repository<Hit> {
+    @Override
+    public void clear() {
+        // todo
+    }
+
+    @Override
+    public void save(Hit hit) {
+        // todo
+    }
+
+    @Override
+    public List<Hit> getAll() {
+        // todo
+        return null;
+    }
+}
